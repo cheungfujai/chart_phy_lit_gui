baf511218b5170266d2b7fc53a9f91612a21d144
Chun Fai Tse <chunfaitse@Chuns-MacBook-Pro.local>
2020-11-17 20:59:40 +0800

    add radio group in page 4

--- src/Component/Page4.tsx
+++ src/Component/Page4.tsx
@@ -22,16 +22,20 @@ export default function Page4() {
         <Box color="text.primary" style={{ padding: "20px", }}>
             <img src={Ipla} alt="Ipla" style={{ paddingBottom: "50px", height:"80px", margin: "auto", display: "block", alignItems: "center"}} />
             <Typography variant="h6" display="block" gutterBottom style={{ padding: "10px" }}>How much time did you usually spend doing any kinds of physical activities on one of those days (in hours and minutes, e.g. 2 hours and 30 minutes)?</Typography>
-            <TextField id="outlined-basic" label="" variant="outlined" />
+            <TextField id="outlined-basic" label="" style={{padding: "20px", paddingBottom: "30px" }} variant="outlined" />
             
+            <Typography variant="h6" display="block" gutterBottom >Highest level of sports participation</Typography>
+
             <RadioGroup aria-label="sportParticipation" value={sportParticipation} onChange={handleSportParticipationChange} style={{ padding: "10px" }}>
                 <FormControlLabel value="Active starts" control={<Radio />} label="Active starts" />
                 <FormControlLabel value="Fundamentals" control={<Radio />} label="Fundamentals" />
-                <FormControlLabel value="male" control={<Radio />} label="Male" />
-                <FormControlLabel value="male" control={<Radio />} label="Male" />
-                <FormControlLabel value="male" control={<Radio />} label="Male" />
-                <FormControlLabel value="male2" control={<Radio />} label="Male2" />
-                <FormControlLabel value="male" control={<Radio />} label="Male" />
+                <FormControlLabel value="Learn to train" control={<Radio />} label="Learn to train" />
+                <FormControlLabel value="Train to train" control={<Radio />} label="Train to train" />
+                <FormControlLabel value="Train to compete" control={<Radio />} label="Train to compete" />
+                <FormControlLabel value="Train to win" control={<Radio />} label="Train to win" />
+                <FormControlLabel value="Competitive for life" control={<Radio />} label="Cometitive for life" />
+                <FormControlLabel value="Fit for life" control={<Radio />} label="Fit for life" />
+                <FormControlLabel value="Active for life" control={<Radio />} label="Active for life" />
             </RadioGroup>
             
         </Box>
